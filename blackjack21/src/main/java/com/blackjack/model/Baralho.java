b390dd00a3fa48158af31937c248ea927036cb7d
jefersonarthur123 <jefersonsoareslauriano@gmail.com>
2026-08-25 09:22:24 -0300

    Add files via upload

---
+++ blackjack21/src/main/java/com/blackjack/model/Baralho.java
@@ -0,0 +1,23 @@
+
+package com.blackjack.model;
+
+import java.util.Collections;
+import java.util.Stack;
+
+public class Baralho {
+    private final Stack<Carta> cartas = new Stack<>();
+
+    public void reiniciar() {
+        cartas.clear();
+        for (Nipe n : Nipe.values()) {
+            for (Rank r : Rank.values()) {
+                cartas.push(new Carta(n, r));
+            }
+        }
+        Collections.shuffle(cartas); // O shuffle do Java é mais otimizado que o manual
+    }
+
+    public Carta comprar() {
+        return cartas.isEmpty() ? null : cartas.pop();
+    }
+}
